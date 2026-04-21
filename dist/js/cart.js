0d378d40b9f14be48ca1d053daab3481e8fc0a98
Maciej Wlosek <wlomcj@gmail.com>
2026-04-21 15:22:49 +0200

    feat(cart): enhance cart functionality with item management and calculations
    - Implemented rendering of cart items with dynamic quantity updates.
    - Added subtotal, discount, shipping, and total calculations.
    - Introduced clear cart and checkout button functionalities.
    - Updated cart HTML structure to accommodate new features.
    - Added delete icon for removing items from the cart.
    - Improved styling for cart components and buttons.

--- dist/js/cart.js
+++ dist/js/cart.js
@@ -21,45 +21,126 @@ document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, vo
     renderCartItem(products);
     console.log(products);
 }));
+const cartEmpty = document.getElementById("cart-content");
 const cartItem = document.getElementById("cart-table-items");
+const subTotal = document.getElementById("cart-sub-total");
+const total = document.getElementById("cart-total");
+const shipping = document.getElementById("cart-shipping");
+const discount = document.getElementById("cart-discount");
+let subTotalValue = 0;
+let shippingValue = 30;
+let totalValue = 0;
+let discountValue = 0;
+let discountStage = 3000;
+let discountPercentage = 0.1;
 function renderCartItem(products) {
-    const localStorageItems = JSON.parse(localStorage.getItem("cart"));
+    const localStorageItems = JSON.parse(localStorage.getItem("cart")) || [];
     const result = localStorageItems.map((res) => {
         const cartIds = products.find((prod) => prod.id === res.storageId);
         return Object.assign(Object.assign({}, cartIds), { quantity: res.quantity });
     });
-    console.log("res", result);
-    return (cartItem.innerHTML = `<div>${result
-        .map((res) => {
-        const { name, price, imageUrl, quantity } = res;
-        return `
-      <tr
-                  class="text-title text-title--sm text-title--bold cart__table-row"
-                >
-                  <td class="cart__table-cell">
-                    <div class="cart__image">
-                      <img src="${imageUrl}" />
-                    </div>
-                  </td>
-                  <td class="cart__table-cell">${name}</td>
-                  <td class="cart__table-cell">$${price}</td>
-                  <td class="cart__table-cell">
-                    <div class="cart__buttons">
-                      <button class="button button--inverse">
-                        <span class="text-body text-body--md">&minus;</span>
-                      </button>
-                      <span class="text-body text-body--md">${quantity}</span
-                      ><button class="button button--inverse">
-                        <span class="text-body text-body--md">+</span>
-                      </button>
-                    </div>
-                  </td>
-                  <td class="cart__table-cell">$${price * quantity}</td>
-                  <td class="cart__table-cell">CION</td>
-                </tr>
-      
-      
-      `;
-    })
-        .join("")}`);
+    //Calculate Sub Value
+    subTotalValue = result.reduce((total, item) => {
+        return total + item.price * item.quantity;
+    }, 0);
+    //Calculate Discount Value
+    if (subTotalValue >= discountStage) {
+        discountValue = subTotalValue * discountPercentage;
+    }
+    //Calculate Total Value
+    totalValue = subTotalValue - discountValue + shippingValue;
+    //Value assigne
+    shipping === null || shipping === void 0 ? void 0 : shipping.innerHTML = `$${shippingValue}`;
+    discount === null || discount === void 0 ? void 0 : discount.innerHTML = `$${discountValue}`;
+    subTotal === null || subTotal === void 0 ? void 0 : subTotal.innerHTML = `$${subTotalValue}`;
+    total === null || total === void 0 ? void 0 : total.innerHTML = `$${totalValue}`;
+    if (result.length > 0) {
+        cartItem.innerHTML = result
+            .map((res) => {
+            const { id, name, price, imageUrl, quantity } = res;
+            return `
+          <tr class="text-title text-title--sm text-title--bold cart__table-row">
+            <td class="cart__table-cell">
+              <div class="cart__image">
+                <img src="${imageUrl}" alt="${name}" />
+              </div>
+            </td>
+            <td class="cart__table-cell">${name}</td>
+            <td class="cart__table-cell">$${price}</td>
+            <td class="cart__table-cell">
+              <div class="cart__buttons">
+                <button data-id="${id}" class="button button--inverse cart__quantity-sub-button">
+                  <span class="text-body text-body--md ">&minus;</span>
+                </button>
+                <span class="text-body text-body--md">${quantity}</span>
+                <button data-id="${id}" class="button button--inverse cart__quantity-add-button">
+                  <span class="text-body text-body--md">+</span>
+                </button>
+              </div>
+            </td>
+            <td class="cart__table-cell">$${price * quantity}</td>
+            <td class="cart__table-cell">
+              <svg data-id="${id}" class="cart__table-cell-icon">
+                <use href="/src/assets/icons/icons.svg#icon-delete"></use>
+              </svg>
+            </td>
+          </tr>
+        `;
+        })
+            .join("");
+    }
+    else {
+        cartEmpty === null || cartEmpty === void 0 ? void 0 : cartEmpty.innerHTML = `
+    <h2 class= "text-title text-title--lg text-title--primary">
+    Your cart is empty. Use the catalog to add new items.
+    </h2>`;
+    }
 }
+//Event Clear Button
+const clearButtonCart = document.getElementById("cart-button-clear");
+clearButtonCart === null || clearButtonCart === void 0 ? void 0 : clearButtonCart.addEventListener("click", () => {
+    localStorage.removeItem("cart");
+});
+//Event Checkout Button
+const checkoutButtonCart = document.getElementById("cart-button-checkout");
+checkoutButtonCart === null || checkoutButtonCart === void 0 ? void 0 : checkoutButtonCart.addEventListener("click", () => {
+    //   localStorage.removeItem("cart");
+    console.log("Thank you for your purchase.");
+});
+cartItem === null || cartItem === void 0 ? void 0 : cartItem.addEventListener("click", (e) => {
+    const removeButton = e.target.closest(".cart__table-cell-icon");
+    const quantityAddButton = e.target.closest(".cart__quantity-add-button");
+    const quantitySubButton = e.target.closest(".cart__quantity-sub-button");
+    const localStorageItems = JSON.parse(localStorage.getItem("cart"));
+    //Event remove item
+    if (removeButton) {
+        const idRemoveButton = removeButton.dataset.id;
+        const updateCart = localStorageItems.filter((item) => item.storageId !== idRemoveButton);
+        localStorage.setItem("cart", JSON.stringify(updateCart));
+        return;
+    }
+    //Event addQuanity
+    if (quantityAddButton) {
+        const idQuantityAddButton = quantityAddButton.dataset.id;
+        const updatedCart = localStorageItems.map((item) => {
+            if (item.storageId === idQuantityAddButton) {
+                return Object.assign(Object.assign({}, item), { quantity: item.quantity + 1 });
+            }
+            return item;
+        });
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+        return;
+    }
+    //Event subQuanity
+    if (quantitySubButton) {
+        const idQuantitySubButton = quantitySubButton.dataset.id;
+        const updatedCart = localStorageItems.map((item) => {
+            if (item.storageId === idQuantitySubButton) {
+                return Object.assign(Object.assign({}, item), { quantity: item.quantity - 1 });
+            }
+            return item;
+        });
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+        return;
+    }
+});
--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -15,48 +15,140 @@ document.addEventListener("DOMContentLoaded", async () => {
   console.log(products);
 });
 
+const cartEmpty = document.getElementById("cart-content");
 const cartItem = document.getElementById("cart-table-items");
+const subTotal = document.getElementById("cart-sub-total");
+const total = document.getElementById("cart-total");
+const shipping = document.getElementById("cart-shipping");
+const discount = document.getElementById("cart-discount");
+
+let subTotalValue = 0;
+let shippingValue = 30;
+let totalValue = 0;
+let discountValue = 0;
+let discountStage = 3000;
+let discountPercentage = 0.1;
 
 function renderCartItem(products) {
-  const localStorageItems = JSON.parse(localStorage.getItem("cart"));
+  const localStorageItems = JSON.parse(localStorage.getItem("cart")) || [];
 
   const result = localStorageItems.map((res) => {
     const cartIds = products.find((prod) => prod.id === res.storageId);
     return { ...cartIds, quantity: res.quantity };
   });
 
-  console.log("res", result);
-  return (cartItem.innerHTML = `<div>${result
-    .map((res) => {
-      const { name, price, imageUrl, quantity } = res;
-      return `
-      <tr
-                  class="text-title text-title--sm text-title--bold cart__table-row"
-                >
-                  <td class="cart__table-cell">
-                    <div class="cart__image">
-                      <img src="${imageUrl}" />
-                    </div>
-                  </td>
-                  <td class="cart__table-cell">${name}</td>
-                  <td class="cart__table-cell">$${price}</td>
-                  <td class="cart__table-cell">
-                    <div class="cart__buttons">
-                      <button class="button button--inverse">
-                        <span class="text-body text-body--md">&minus;</span>
-                      </button>
-                      <span class="text-body text-body--md">${quantity}</span
-                      ><button class="button button--inverse">
-                        <span class="text-body text-body--md">+</span>
-                      </button>
-                    </div>
-                  </td>
-                  <td class="cart__table-cell">$${price * quantity}</td>
-                  <td class="cart__table-cell">CION</td>
-                </tr>
-      
-      
-      `;
-    })
-    .join("")}`);
+  //Calculate Sub Value
+  subTotalValue = result.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
+  //Calculate Discount Value
+  if (subTotalValue >= discountStage) {
+    discountValue = subTotalValue * discountPercentage;
+  }
+
+  //Calculate Total Value
+  totalValue = subTotalValue - discountValue + shippingValue;
+
+  //Value assigne
+  shipping?.innerHTML = `$${shippingValue}`;
+  discount?.innerHTML = `$${discountValue}`;
+  subTotal?.innerHTML = `$${subTotalValue}`;
+  total?.innerHTML = `$${totalValue}`;
+
+  if (result.length > 0) {
+    cartItem.innerHTML = result
+      .map((res) => {
+        const { id, name, price, imageUrl, quantity } = res;
+        return `
+          <tr class="text-title text-title--sm text-title--bold cart__table-row">
+            <td class="cart__table-cell">
+              <div class="cart__image">
+                <img src="${imageUrl}" alt="${name}" />
+              </div>
+            </td>
+            <td class="cart__table-cell">${name}</td>
+            <td class="cart__table-cell">$${price}</td>
+            <td class="cart__table-cell">
+              <div class="cart__buttons">
+                <button data-id="${id}" class="button button--inverse cart__quantity-sub-button">
+                  <span class="text-body text-body--md ">&minus;</span>
+                </button>
+                <span class="text-body text-body--md">${quantity}</span>
+                <button data-id="${id}" class="button button--inverse cart__quantity-add-button">
+                  <span class="text-body text-body--md">+</span>
+                </button>
+              </div>
+            </td>
+            <td class="cart__table-cell">$${price * quantity}</td>
+            <td class="cart__table-cell">
+              <svg data-id="${id}" class="cart__table-cell-icon">
+                <use href="/src/assets/icons/icons.svg#icon-delete"></use>
+              </svg>
+            </td>
+          </tr>
+        `;
+      })
+      .join("");
+  } else {
+    cartEmpty?.innerHTML = `
+    <h2 class= "text-title text-title--lg text-title--primary">
+    Your cart is empty. Use the catalog to add new items.
+    </h2>`;
+  }
 }
+
+//Event Clear Button
+const clearButtonCart = document.getElementById("cart-button-clear");
+clearButtonCart?.addEventListener("click", () => {
+  localStorage.removeItem("cart");
+});
+
+//Event Checkout Button
+const checkoutButtonCart = document.getElementById("cart-button-checkout");
+checkoutButtonCart?.addEventListener("click", () => {
+  //   localStorage.removeItem("cart");
+  console.log("Thank you for your purchase.");
+});
+
+cartItem?.addEventListener("click", (e) => {
+  const removeButton = e.target.closest(".cart__table-cell-icon");
+  const quantityAddButton = e.target.closest(".cart__quantity-add-button");
+  const quantitySubButton = e.target.closest(".cart__quantity-sub-button");
+
+  const localStorageItems = JSON.parse(localStorage.getItem("cart"));
+
+  //Event remove item
+  if (removeButton) {
+    const idRemoveButton = removeButton.dataset.id;
+    const updateCart = localStorageItems.filter(
+      (item) => item.storageId !== idRemoveButton,
+    );
+    localStorage.setItem("cart", JSON.stringify(updateCart));
+    return;
+  }
+  //Event addQuanity
+  if (quantityAddButton) {
+    const idQuantityAddButton = quantityAddButton.dataset.id;
+    const updatedCart = localStorageItems.map((item) => {
+      if (item.storageId === idQuantityAddButton) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    return;
+  }
+  //Event subQuanity
+  if (quantitySubButton) {
+    const idQuantitySubButton = quantitySubButton.dataset.id;
+    const updatedCart = localStorageItems.map((item) => {
+      if (item.storageId === idQuantitySubButton) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    return;
+  }
+});
